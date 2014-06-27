53b21377dbdd05dd581847860c7ed3e8e0a0c43e
Nikhil <nikhil@menghani.com>
2014-06-28 00:43:41 +0530

    added afzc protocols

---
+++ src/flashablezipcreator/AFZC/Protocols.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flashablezipcreator.AFZC;
+
+/**
+ *
+ * @author Nikhil
+ */
+public class Protocols {
+    public static void sopln(String print){
+        sop(print + "\n");
+    }
+    public static void sop(String print){
+        System.out.print(print);
+    }
+}
